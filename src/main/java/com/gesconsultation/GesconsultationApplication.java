5e2dcdd3e421b124a5513bb3ac088c0fe6a64217
oulimata <jadaliou3@gmail.com>
2019-12-01 17:48:23 +0000

    initialisation

---
+++ src/main/java/com/gesconsultation/GesconsultationApplication.java
@@ -0,0 +1,13 @@
+package com.gesconsultation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesconsultationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GesconsultationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gesconsultation/GesconsultationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gesconsultation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesconsultationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
